c9d10ddb3c6912cee22513d884e0f336d056beaf
Martin Peralez <martin.peralez@outlook.com>
2021-03-13 21:55:47 -0600

    Created Color Palette for console game Created different consoles that will display different information Created a base map Created a player object with a visible Field-of-View

---
+++ RogueSharp/CSharpRPG/Core/Colors.cs
@@ -0,0 +1,25 @@
+﻿using RLNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRPG.Core
+{
+    class Colors
+    {
+        public static RLColor FLoorBackground = RLColor.Black;
+        public static RLColor FLoor = Swatch.AlternateDarkest;
+        public static RLColor FloorBackgroundFov = Swatch.DbDark;
+        public static RLColor FLoorFov = Swatch.Alternate;
+
+        public static RLColor WallBackground = Swatch.SecondaryDarkest;
+        public static RLColor Wall = Swatch.Secondary;
+        public static RLColor WallBackgroundFov = Swatch.SecondaryDarker;
+        public static RLColor WallFov = Swatch.SecondaryLighter;
+
+        public static RLColor TextHeading = Swatch.DbLight;
+        public static RLColor Player = Swatch.DbLight;
+    }
+}
